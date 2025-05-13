3d114f8bdbc53ddc680b8a65cb7f0704e142bff5
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-05-13 12:03:22 +0200

    Implemented S-expression type

---
+++ src/sexp.rs
@@ -0,0 +1,41 @@
+use core::fmt;
+
+
+pub enum Sexp {
+    Integer(i64),
+    Symbol(u64),
+    String(String),
+    Pair(Box<Sexp>, Box<Sexp>),
+    Nil
+}
+
+impl fmt::Display for Sexp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Sexp::Integer(i) => write!(f, "{}", i),
+            Sexp::Symbol(s) => write!(f, "{}", s),
+            Sexp::String(s) => write!(f, "{:?}", s),
+            Sexp::Pair(car, cdr) => {
+                write!(f, "(")?;
+                write!(f, "{}", car)?;
+                let mut it = cdr.as_ref();
+                loop {
+                    match it {
+                        Sexp::Pair(car, cdr) => {
+                            write!(f, " {}", car)?;
+                            it = &cdr;
+                        },
+                        Sexp::Nil => break,
+                        s => {
+                            write!(f, " . {}", s)?;
+                            break;
+                        }
+
+                    }
+                }
+                write!(f, ")")
+            },
+            Sexp::Nil => write!(f, "()")
+        }
+    }
+}
